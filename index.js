0b6ff643fec4f913b7be6ee1b7796c027eeffefe
Jane Chapco <jane.chapco@gmail.com>
2026-05-05 14:36:04 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+  # Rules:
+    1. Atleast 8 characters
+    2. Atleast one lowercase character
+    3. Atleast one uppercase character
+    4. Atleast one number
+    5. Atleast one special character (!, $)
+    6. Should not contain the substring 'test', 'password'
+
+  # Category:
+    - Strong (6/6)
+    - Moderate (4-5/6)
+    - Weak (0-3/6)
+*/
